aff6c6bc07e8ccba461b4f6f84bc97c97d2dd435
team11415 <stormsurgefrc@gmail.com>
2026-02-26 19:36:19 -0500

    added 2nd auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -78,6 +79,9 @@ public class RobotContainer {
                         spindexer, shooter
                     ).withTimeout(4.0)
                 );
+
+        NamedCommands.registerCommand("runWait2", new WaitCommand(2.0));
+        NamedCommands.registerCommand("runWait4", new WaitCommand(4.0));
 
         NamedCommands.registerCommand("stopAll",
             Commands.runOnce(() -> { spindexer.stopAll(); shooter.stopShooter(); }, spindexer, shooter)
